0731184e551de9a76e2ae21d3c80c8e9ea82e2e5
zhang.rongjun <zhang.rongjun@oe.21vianet.com>
2021-12-07 10:06:32 +0800

    删除密钥

---
+++ kubernetes_demo/src/main/java/com/example/kubernetes_demo/KubernetesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kubernetes_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesDemoApplication.class, args);
+    }
+
+}
---
+++ kubernetes_demo/src/test/java/com/example/kubernetes_demo/KubernetesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kubernetes_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
